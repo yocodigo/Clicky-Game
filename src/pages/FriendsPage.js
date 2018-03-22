691d56039ef82bc882ba484d771c834895fdb3de
unknown <jjhernandez94@yahoo.com>
2018-03-22 06:13:14 -0700

    modified randomOrder function in FriendsPage.js

--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -9,11 +9,12 @@ class FriendsPage extends Component {
     friends
   };
 
-  randomOrder = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+  randomOrder = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i+1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   };
 
     render() {
